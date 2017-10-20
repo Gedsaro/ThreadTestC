a1f7865eef727498e2741d9aea1f2d7632fc5062
Mason Frith <gedsaro@hotmail.com>
2017-10-20 15:04:58 -0600

    Created this thread example project to test C# threads

--- ThreadTestC/Program.cs
+++ ThreadTestC/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadTestC
@@ -10,6 +11,21 @@ namespace ThreadTestC
     {
         static void Main(string[] args)
         {
+
+            WritingThread wt = new WritingThread();
+
+            Thread t = new Thread(new ThreadStart(wt.displayX));
+            t.Start();
+
+            for(int i =0; i<10; i++)
+            {
+                Thread.Sleep(300);
+                wt.x = i;
+            }
+
+            Thread.Sleep(1500);
+            t.Abort();
+
         }
     }
 }
---
+++ ThreadTestC/WritingThread.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class WritingThread
+{
+    public int x;
+
+    public WritingThread()
+    {
+        this.x = 0;
+    }
+
+    public void displayX()
+    {
+        do
+        {
+
+            Console.WriteLine(x);
+        } while (this.x <= 9);
+
+        Console.WriteLine("Done writing threads");
+    }
+
+}
+
